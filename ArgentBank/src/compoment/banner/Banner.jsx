76da1fd89e73649011a37ea6ceccab760a68b013
Szmoniewski Baptiste <151198403+szmo-gif@users.noreply.github.com>
2024-08-20 18:17:48 +0200

    banner

--- ArgentBank/src/compoment/banner/Banner.jsx
+++ ArgentBank/src/compoment/banner/Banner.jsx
@@ -1,5 +1,17 @@
+import banner from "../../assets/image/bank-tree.jpeg"
+
+import './Banner.css'
+
 export default function Banner() {
   return (
-    <div>Banner</div>
+    <div class="hero">
+    <section class="hero-content">
+      <h2 class="sr-only">Promoted Content</h2>
+      <p class="subtitle">No fees.</p>
+      <p class="subtitle">No minimum deposit.</p>
+      <p class="subtitle">High interest rates.</p>
+      <p class="text">Open a savings account with Argent Bank today!</p>
+    </section>
+  </div>
   )
 }
