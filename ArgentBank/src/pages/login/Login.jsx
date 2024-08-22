87e95c14c94656e7b3e6ae7f14d29e8daa942f8c
Szmoniewski Baptiste <151198403+szmo-gif@users.noreply.github.com>
2024-08-22 14:28:41 +0200

    login page

--- ArgentBank/src/pages/login/Login.jsx
+++ ArgentBank/src/pages/login/Login.jsx
@@ -1,5 +1,35 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+
+import './Login.css';
+
 export default function Login() {
   return (
-    <h1>Login</h1>
+    <main className="bg-dark">
+      <section className="sign-in-content">
+        <FontAwesomeIcon icon={faUserCircle} className='fa-user-circle sign-in-icon' />
+
+        <h1>Sign In</h1>
+
+        <form>
+          <div className="input-wrapper">
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" name="username" />
+          </div>
+
+          <div className="input-wrapper">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" />
+          </div>
+
+          <div className="input-remember">
+            <input type="checkbox" id='remember-me' />
+            <label htmlFor="remember-me">Remember me</label>
+          </div>
+
+          <button type="submit" className="sign-in-button">Sign In</button>
+        </form>
+      </section>
+    </main>
   )
 }
