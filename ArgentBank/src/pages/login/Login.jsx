df670f4b2c870b1a693e94af282e588fede11a98
Szmoniewski Baptiste <151198403+szmo-gif@users.noreply.github.com>
2024-08-26 10:55:11 +0200

    login

--- ArgentBank/src/pages/login/Login.jsx
+++ ArgentBank/src/pages/login/Login.jsx
@@ -3,7 +3,40 @@ import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
 import './Login.css';
 
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 export default function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+  
+  const handleSubmit = async(event) => {
+    event.preventDefault();
+
+    try {
+      const response = fetch('http://localhost:3001/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+      
+      const data = await response.json();
+
+      if (response.ok) {
+        navigate('/Profile');
+      } else {
+        setError(data.message);
+      } 
+    } catch (error) {
+      console.error('erreur de connexion', error);
+      setError('erreur de connexion');
+    }
+      }
+
   return (
     <main className="bg-dark">
       <section className="sign-in-content">
@@ -11,15 +44,25 @@ export default function Login() {
 
         <h1>Sign In</h1>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="input-wrapper">
             <label htmlFor="username">Username</label>
-            <input type="text" id="username" name="username" />
+            <input 
+            type="text"
+            id="username"
+            name="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}/>
           </div>
 
           <div className="input-wrapper">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" name="password" />
+            <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}/>
           </div>
 
           <div className="input-remember">
